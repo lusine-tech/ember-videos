d70c75bfbfe9b63a10de4631361948a670ad9d85
Lusine <lyeremyan@griddynamics.com>
2022-09-13 13:45:58 +0400

    ready 3

--- tests/unit/routes/cart-test.js
+++ tests/unit/routes/cart-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'emberjs-tutorial/tests/helpers';
 
-module('Unit | Route | clothes/index', function (hooks) {
+module('Unit | Route | cart', function (hooks) {
   setupTest(hooks);
 
   test('it exists', function (assert) {
-    let route = this.owner.lookup('route:clothes/index');
+    let route = this.owner.lookup('route:cart');
     assert.ok(route);
   });
 });
